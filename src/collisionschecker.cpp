e1112ccb8eb1476d45a33d2eb1f45cbcb9ce6d40
Richard Plangger <Richard.Plangger@gmx.net>
2011-01-15 16:53:04 +0100

    ...

--- src/collisionschecker.cpp
+++ src/collisionschecker.cpp
@@ -17,7 +17,8 @@ bool CollisionsChecker::collidate(float x, float y, float w, float h, float rot,
     plane1.prototype.push_back(new vec(x + w,y));
     plane1.prototype.push_back(new vec(x + w,y + h));
     plane1.prototype.push_back(new vec(x,y + h));
-
+    plane1.pivot = vec(x + w / 2, y + h / 2);
+    plane1.apply();
 
     geomfig plane2;
     plane2.angle = rot1;
@@ -26,7 +27,8 @@ bool CollisionsChecker::collidate(float x, float y, float w, float h, float rot,
     plane2.prototype.push_back(new vec(x1 + w1,y1));
     plane2.prototype.push_back(new vec(x1 + w1,y1 + h1));
     plane2.prototype.push_back(new vec(x1,y1 + h1));
-
+    plane2.pivot = vec(x1 + w1 / 2, y1 + h1 / 2);
+    plane2.apply();
 
     return false;
 }
