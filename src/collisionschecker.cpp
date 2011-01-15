c2e1c94bd6fd273615412b0fe0f9b6294b2b70a2
Richard Plangger <Richard.Plangger@gmx.net>
2011-01-15 16:51:43 +0100

    ...

--- src/collisionschecker.cpp
+++ src/collisionschecker.cpp
@@ -10,6 +10,23 @@ CollisionsChecker::CollisionsChecker(QObject *parent) :
 bool CollisionsChecker::collidate(float x, float y, float w, float h, float rot, float x1, float y1, float w1, float h1, float rot1) {
 
     //check collision here
+    geomfig plane1;
+    plane1.angle = rot;
+    plane1.scale = 1;
+    plane1.prototype.push_back(new vec(x,y));
+    plane1.prototype.push_back(new vec(x + w,y));
+    plane1.prototype.push_back(new vec(x + w,y + h));
+    plane1.prototype.push_back(new vec(x,y + h));
+
+
+    geomfig plane2;
+    plane2.angle = rot1;
+    plane2.scale = 1;
+    plane2.prototype.push_back(new vec(x1,y1));
+    plane2.prototype.push_back(new vec(x1 + w1,y1));
+    plane2.prototype.push_back(new vec(x1 + w1,y1 + h1));
+    plane2.prototype.push_back(new vec(x1,y1 + h1));
+
 
     return false;
 }
